2643085fdd35ba012cbfe8fc0615594ecdab16fe
MichaelDaum <daum@michaeldaumconsulting.com>
2015-07-08 14:10:59 +0200

    initial checkin

---
+++ lib/Foswiki/Contrib/WebFontsContrib.pm
@@ -0,0 +1,28 @@
+# Extension for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+#
+# WebFontsContrib is Copyright (C) 2015 Michael Daum http://michaeldaumconsulting.com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details, published at
+# http://www.gnu.org/copyleft/gpl.html
+
+package Foswiki::Contrib::WebFontsContrib;
+
+use strict;
+use warnings;
+
+use Foswiki::Func ();
+
+our $VERSION = '1.00';
+our $RELEASE = '1.00';
+our $SHORTDESCRIPTION = 'Web fonts for Foswiki skins';
+our $NO_PREFS_IN_TOPIC = 1;
+
+1;
