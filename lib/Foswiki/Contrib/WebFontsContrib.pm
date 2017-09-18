60649fd3967dcfd75984b3efb702ef391945398e
MichaelDaum <daum@michaeldaumconsulting.com>
2017-09-18 10:28:05 +0200

    Item14483: added missing js and css files

--- lib/Foswiki/Contrib/WebFontsContrib.pm
+++ lib/Foswiki/Contrib/WebFontsContrib.pm
@@ -1,6 +1,6 @@
 # Extension for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
 #
-# WebFontsContrib is Copyright (C) 2015-2016 Michael Daum http://michaeldaumconsulting.com
+# WebFontsContrib is Copyright (C) 2015-2017 Michael Daum http://michaeldaumconsulting.com
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -20,8 +20,8 @@ use warnings;
 
 use Foswiki::Func ();
 
-our $VERSION = '2.00';
-our $RELEASE = '30 Nov 2016';
+our $VERSION = '2.01';
+our $RELEASE = '18 Sep 2017';
 our $SHORTDESCRIPTION = 'Web fonts for Foswiki skins';
 our $NO_PREFS_IN_TOPIC = 1;
 
---
+++ pub/System/MaterialIcons/script.js
@@ -0,0 +1,35 @@
+"use strict";
+jQuery(function($) {
+   var timer;
+   function filterOverview(val) {
+      var regex = new RegExp(val, "i"),
+         $overview = $(".overview");
+      console.log("filterOverview(",val,")");
+      if (val != '') {
+         $overview.find(".entry").hide().each(function() {
+            var $this = $(this);
+            if (regex.test($this.text())) {
+               $this.show();
+            }
+         });
+      } else {
+         $overview.find(".entry").show();
+      }
+   }
+   $(".filterIcons:not(.filterIconsInited").livequery(function() {
+      var $input = $(this);
+      $input.addClass("filterIconsInited");
+      $input.keyup(function() {
+         if (typeof(timer) !== 'undefined') {
+            //console.log("clearing timeout");
+            window.clearTimeout(timer);
+         }
+         timer = window.setTimeout(function() {
+            var val = $input.val();
+            filterOverview(val);
+            timer = undefined;
+         }, 100);
+         //console.log("new timer",timer);
+      });
+   });
+});
